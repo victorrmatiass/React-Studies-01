8934d92d1af9138c982a9a05ae363dc285b510c0
Victor Matias <victormatiasilva@gmail.com>
2023-03-18 23:30:17 -0300

    class 04 - class and ciclylife

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+import { Component } from 'react';
 import './App.css';
+
+class Count extends Component {
+  constructor() {
+    super()
+    this.state = { count: 0}
+    this.countButton = this.countButton.bind(this)
+  }
+
+  countButton(){
+    if (this.state.count < 20) {
+      this.setState({count: this.state.count + 1})
+    }
+  }
+
+  render() {
+
+    const { label } = this.props;    
+    const { count } = this.state
+
+    return (
+      <div>
+        <Button label='Click' onClick={this.countButton}/>
+        <p>
+          { label }: {count}
+        </p>
+      </div>
+    )
+  }
+
+}
 
 function Button({ label, onClick, className='button--primary'}) {
   return (
@@ -21,6 +52,7 @@ function App() {
       <h1>Hello World!</h1>
       <Button label="Sing Up" onClick={UserRegistred} className='button--primary' />
       <Button label="Sing In" onClick={UserLogged} className='button--secondary' />
+      <Count label="Clicks"/>
     </div>
   );
 }
